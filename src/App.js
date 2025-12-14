cf993bbac800232ded727c9f1fe1dabb03fdd114
ewiniarski <ewiniarski@o2.pl>
2025-12-14 11:45:12 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import {useState} from "react";
+
 function App() {
-    let title = 'Wall-E';
+    const [title, setTitle] = useState('');
 
     function handleChange(event) {
-        console.log(event.target.value);
+        setTitle(event.target.value);
+
+    }
+
+    let message = '';
+    if (title.length < 5) {
+        message = 'Title is too short!';
+    } else if (title.length < 10) {
+        message = 'Title is good!';
+    } else if (title.length >= 10) {
+        message = 'Title is too long!';
     }
 
     return (
         <div>
             <h1>My favourite movies to watch</h1>
             <h2>My favourite movie for today is {title}</h2>
-            <input type="text" onChange={handleChange}/>
+            {
+                title.length > 0 && <div>{message}</div>
+            }
+            <input type="text" value={title} onChange={handleChange}/>
         </div>
     );
 }
